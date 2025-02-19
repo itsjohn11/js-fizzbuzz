e265acedcf98e3921199d0078f724e52acaa52db
its.john <giovannialesi87@icloud.com>
2025-02-19 20:25:14 +0100

    fizzBuzz NC

--- js/script.js
+++ js/script.js
@@ -2,7 +2,27 @@
 
 */
 
-for (let i = 1; i <= 100; i++ ) { 
-    console.log(i);
-        
+// for (let i = 1; i <= 100; i++ ) { 
+//     console.log(i); 
+//  }
+
+
+for( let i = 0; i <= 100; i++){
+     let fizzBuzz;
+     if(i % 3 === 0){
+         fizzBuzz = "fizz";
+    }else if(i % 5 === 0){
+         fizzBuzz = "buzz";
+    }else if ( i % 3, i % 5 === 0){
+        fizzBuzz = "fizzBuzz";
+    }
+    console.log( fizzBuzz);
 }
+
+for(let i = 1; i <= 10; i++){
+   const multiply = 5 * i;
+    console.log(`Iterazione $(i): 5 * $(i) = $(multiply)`);    
+}
+
+
+
